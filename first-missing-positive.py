b5720d50613d23f563641d9c12e04108832feae5
RickyHuo <huochen1994@163.com>
2018-08-02 13:46:15 +0800

    Accepted first-missing-positive

--- first-missing-positive.py
+++ first-missing-positive.py
@@ -1,14 +1,16 @@
+# encoding: utf-8
+
+
 class Solution(object):
     def firstMissingPositive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-
+        # 去重
+        nums = list(set(nums))
+        # 排序
         nums = sorted(nums)
-
-        a = [1,23,4]
-        a.sort()
 
         for i in xrange(len(nums)):
             if nums[i] > 0:
@@ -25,5 +27,5 @@ class Solution(object):
 if __name__ == '__main__':
     print Solution().firstMissingPositive([7, 8, 9, 11, 12])
     print Solution().firstMissingPositive([3, 4, -1, 1])
-    print Solution().firstMissingPositive([1, 2, 0])
+    print Solution().firstMissingPositive([0, 1, 1000])
     print Solution().firstMissingPositive([0, 2, 2, 1, 1])
